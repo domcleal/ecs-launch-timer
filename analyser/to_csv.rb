2178fc93a97d1db5d1e0e5f7ea0333b02114c63c
Dominic Cleal <dominic.cleal@freeagent.com>
2019-05-21 09:58:58 +0100

    Add simple CSV export from CW logs

---
+++ analyser/to_csv.rb
@@ -0,0 +1,29 @@
+#!/usr/bin/env ruby
+
+require "aws-sdk-cloudwatchlogs"
+
+cw = Aws::CloudWatchLogs::Client.new(region: "eu-west-1")
+
+# TODO: should loop, will return max of 10k events or 1MB
+logs = cw.get_log_events({
+  log_group_name: "launch-timer",
+  log_stream_name: ARGV.first,
+  start_from_head: true,
+})
+
+header = nil
+logs.events.each do |event|
+  next unless event.message =~ /^timestamps ([\w_= ]+)/
+  times = {}
+  $1.split(" ").each do |kv|
+    key, ts = kv.split("=")
+    times[key] = ts
+  end
+
+  unless header
+    header = times.keys.sort
+    puts header.join(",")
+  end
+
+  puts header.map { |h| times[h] }.join(",")
+end
